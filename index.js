0711969178419078bccee3a93d4d6144c4405323
Petr Valenta <petr.valenta.92@gmail.com>
2024-01-14 23:00:19 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,2 @@
+// Global variable ReactDOM with method render (element to be placed, place where the element is rendered)
+ReactDOM.render(<h1>Now, I'm using React!</h1>, document.getElementById('root'));
