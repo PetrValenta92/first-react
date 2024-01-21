15d33cf971f268a477763cbea3304f5944c02813
Petr Valenta <petr.valenta.92@gmail.com>
2024-01-21 21:20:42 +0100

    Parent/Child Components

--- index.js
+++ index.js
@@ -154,3 +154,51 @@ root.render(navbar);
 // Funftions that return a React elements (UI)
 // Components use PascalCase notation for names
 // e.g. <MyComponent /> 
+
+// ?------------- Parent/Child Components ----------------
+
+/* e.g.
+function Header() {
+    return (
+        <header>
+            <nav>
+                <img src="./react-logo.png" width="40px" />
+            </nav>
+        </header>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            <small>© 2021 Ziroll development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+function MainContent() {
+    return (
+        <div>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's a popular library, so I'll be 
+                able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                if I know React</li>
+            </ol>
+        </div>
+    )
+}
+
+function Page() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"))
+*/
